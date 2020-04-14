1bf64dc67cdb0513e1c4eb427610bf4c8e23f687
manoelnecojr@gmail <necoengenheiro@gmail.com>
2020-04-14 13:13:31 -0300

    up

--- luminati/url.go
+++ luminati/url.go
@@ -7,22 +7,24 @@ type URL struct {
 	user    string
 	pass    string
 	ip      string
+	port    string
 	Session *Session
 }
 
 // CreateURL ...
-func CreateURL(user string, pass string) *URL {
+func CreateURL(user string, pass string, host string, port string) *URL {
 	return &URL{
 		user:    user,
 		pass:    pass,
-		ip:      getHostByName("zproxy.lum-superproxy.io"),
+		ip:      getHostByName(host),
+		port:    port,
 		Session: CreateSession(),
 	}
 }
 
 // ToString ...
 func (u *URL) ToString() string {
-	return u.user + "-session-" + u.Session.Get() + ":" + u.pass + "@" + u.ip + ":22225"
+	return u.user + "-session-" + u.Session.Get() + ":" + u.pass + "@" + u.ip + ":" + u.port
 }
 
 func getHostByName(name string) string {
